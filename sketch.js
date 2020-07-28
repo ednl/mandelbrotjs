4a8f8bf5eddaa387fdfba69b6798582415c18a35
Ewoud Dronkert <ewoud@dronkert.net>
2020-07-28 02:08:48 +0200

    Controls

--- sketch.js
+++ sketch.js
@@ -1,11 +1,11 @@
-const ZOOM = 10;
-const MAXITER = 360;
+const ZOOM = 8;
+const MAXITER = 720;
 const MINX = -2.00, MAXX = 1.00;
 const MINY = -1.25, MAXY = 1.25;
 
 let minx = MINX, maxx = MAXX;
 let miny = MINY, maxy = MAXY;
-let reset, save;
+let panleft, panright, panup, pandown, zoomin, zoomout, reset, save;
 
 function mousePressed() {
 	const px = mouseX;
@@ -13,8 +13,8 @@ function mousePressed() {
 	if (px >= 0 && px < width && py >= 0 && py < height) {
 		const a = map(px, 0, width, minx, maxx);
 		const b = map(py, 0, height, miny, maxy);
-		const len = Math.hypot(a, b);
-		if (len < 2) {
+		const len = a * a + b * b;
+		if (len <= 4) {
 			let factor = 0.5 / ZOOM;
 			const w = (maxx - minx) * factor;
 			const h = (maxy - miny) * factor;
@@ -38,9 +38,9 @@ function mandelbrot() {
 
 			let i = 0;
 			let a = ca, b = cb;
-			let len = Math.hypot(a, b);
-			const alph = max(0, map(len, 0, 2, 255, 15));
-			while (i < MAXITER && len <= 2) {
+			let len = a * a + b * b;
+			const alph = max(0, map(len, 0, 4, 255, 15));
+			while (i < MAXITER && len <= 4) {
 				const a1 = a*a - b*b + ca;
 				const b1 = 2*a*b + cb;
 				if (a1 == a && b1 == b) {
@@ -48,7 +48,7 @@ function mandelbrot() {
 				} else {
 					a = a1;
 					b = b1;
-					len = Math.hypot(a, b);
+					len = a * a + b * b;
 					++i;
 				}
 			}
@@ -73,7 +73,65 @@ function mandelbrot() {
 function setup() {
 	pixelDensity(1);
 	createCanvas(900, 750);
-	colorMode(HSB);
+	colorMode(HSB, MAXITER, 100, 100, 1);
+	panleft = createButton('Go Left');
+	panleft.mousePressed(() => {
+		const x = (maxx + minx) / 2;
+		const w = (maxx - minx) / 3;
+		minx = x - w * 2;
+		maxx = x + w;
+		mandelbrot();
+	});
+	panright = createButton('Go Right');
+	panright.mousePressed(() => {
+		const x = (maxx + minx) / 2;
+		const w = (maxx - minx) / 3;
+		minx = x - w;
+		maxx = x + w * 2;
+		mandelbrot();
+	});
+	panup = createButton('Go Up');
+	panup.mousePressed(() => {
+		const y = (maxy + miny) / 2;
+		const h = (maxy - miny) / 3;
+		miny = y - h * 2;
+		maxy = y + h;
+		mandelbrot();
+	});
+	pandown = createButton('Go Down');
+	pandown.mousePressed(() => {
+		const y = (maxy + miny) / 2;
+		const h = (maxy - miny) / 3;
+		miny = y - h;
+		maxy = y + h * 2;
+		mandelbrot();
+	});
+	zoomin = createButton('Zoom In');
+	zoomin.mousePressed(() => {
+		const zoom = 4;
+		const x0 = (minx + maxx) / 2;
+		const y0 = (miny + maxy) / 2;
+		const w = (maxx - minx) / 2 / zoom;
+		const h = (maxy - miny) / 2 / zoom;
+		minx = x0 - w;
+		maxx = x0 + w;
+		miny = y0 - h;
+		maxy = y0 + h;
+		mandelbrot();
+	});
+	zoomout = createButton('Zoom Out');
+	zoomout.mousePressed(() => {
+		const zoom = 4;
+		const x0 = (minx + maxx) / 2;
+		const y0 = (miny + maxy) / 2;
+		const w = (maxx - minx) / 2 * zoom;
+		const h = (maxy - miny) / 2 * zoom;
+		minx = x0 - w;
+		maxx = x0 + w;
+		miny = y0 - h;
+		maxy = y0 + h;
+		mandelbrot();
+	});
 	reset = createButton('Reset');
 	reset.mousePressed(() => {
 		minx = MINX; maxx = MAXX;
